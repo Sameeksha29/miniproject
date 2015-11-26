4b23cc674b4952f40a462103949399c7eb751a87
Sameeksha29 <sameeksha.2908@gmail.com>
2015-11-26 22:45:16 +0530

    Create miniques2.php

---
+++ miniques2.php
@@ -0,0 +1,387 @@
+
+<html>
+<body>
+
+<h1>Baby Names</h1>
+<head>
+<style type="text/css">
+input { margin: 0 auto; width: 180px; }
+body {
+	background-image:url("k.jpg");
+	background-repeat:none;
+}
+table {
+	border:3px solid black;margin-left: auto;
+margin-right: auto;
+}
+tr {
+background-color:#E0FFFF	;
+}
+td {border:2px solid black;padding:5px; width: 50px;
+    vertical-align: bottom;
+}
+
+P { text-align: center }
+
+form {
+  text-align: center;
+}
+h1 {text-align:center;font-family:broadway;color:#7B68EE	;}
+
+</style>
+</head>
+<body>
+
+<form  method="GET">
+<p><strong>You want baby names for:</strong></p>
+<select name="gender">
+<option value="Girl">Baby Girl</option>
+<option value="Boy">Baby Boy</option>
+<option value="both">Both</option>
+</select><br><br>
+
+<p><strong>Choose the year-:</strong></p>
+<select name="year">
+<option value="baby1944">1944</option>
+<option value="baby1945">1945</option>
+<option value="baby1946">1946</option>
+<option value="baby1947">1947</option>
+<option value="baby1948">1948</option>
+<option value="baby1949">1949</option>
+<option value="baby1950">1950</option>
+<option value="baby1951">1951</option>
+<option value="baby1952">1952</option>
+<option value="baby1953">1953</option>
+<option value="baby1954">1954</option>
+<option value="baby1955">1955</option>
+<option value="baby1956">1956</option>
+<option value="baby1957">1957</option>
+<option value="baby1958">1958</option>
+<option value="baby1959">1959</option>
+<option value="baby1960">1960</option>
+<option value="baby1961">1961</option>
+<option value="baby1962">1962</option>
+<option value="baby1963">1963</option>
+<option value="baby1964">1964</option>
+<option value="baby1965">1965</option>
+<option value="baby1966">1966</option>
+<option value="baby1967">1967</option>
+<option value="baby1968">1968</option>
+<option value="baby1969">1969</option>
+<option value="baby1970">1970</option>
+<option value="baby1971">1971</option>
+<option value="baby1972">1972</option>
+<option value="baby1973">1973</option>
+<option value="baby1974">1974</option>
+<option value="baby1975">1975</option>
+<option value="baby1976">1976</option>
+<option value="baby1977">1977</option>
+<option value="baby1978">1978</option>
+<option value="baby1979">1979</option>
+<option value="baby1980">1980</option>
+<option value="baby1981">1981</option>
+<option value="baby1982">1982</option>
+<option value="baby1983">1983</option>
+<option value="baby1984">1984</option>
+<option value="baby1985">1985</option>
+<option value="baby1986">1986</option>
+<option value="baby1987">1987</option>
+<option value="baby1988">1988</option>
+<option value="baby1989">1989</option>
+<option value="baby1990">1990</option>
+<option value="baby1991">1991</option>
+<option value="baby1992">1992</option>
+<option value="baby1993">1993</option>
+<option value="baby1994">1994</option>
+<option value="baby1995">1995</option>
+<option value="baby1996">1996</option>
+<option value="baby1997">1997</option>
+<option value="baby1998">1998</option>
+<option value="baby1999">1999</option>
+<option value="baby2000">2000</option>
+<option value="baby2001">2001</option>
+<option value="baby2002">2002</option>
+<option value="baby2003">2003</option>
+<option value="baby2004">2004</option>
+<option value="baby2005">2005</option>
+<option value="baby2006">2006</option>
+<option value="baby2007">2007</option>
+<option value="baby2008">2008</option>
+<option value="baby2009">2009</option>
+<option value="baby2010">2010</option>
+<option value="baby2011">2011</option>
+<option value="baby2012">2012</option>
+<option value="baby2013">2013</option>
+</select><br><br>
+
+
+
+<p><strong>Choose the numbers of names you want-:</strong></p>
+
+<select name="num">
+<option value=10>top 10</option>
+<option value=20>top 20</option>
+<option value=30>top 30</option>
+<option value=40>top 40</option>
+<option value=50>top 50</option>
+<option value=60>top 60</option>
+<option value=70>top 70</option>
+<option value=80>top 80</option>
+<option value=90>top 90</option>
+<option value=100>top 100</option>
+</select><br><br>
+
+<input type="submit" value="Submit!!">
+
+<p><strong>Enter name</strong></p>
+  <input type="text" name="nm">
+<br>
+<select name="g">
+<option value="Girl">Baby Girl</option>
+<option value="Boy">Baby Boy</option>
+<option value="both">Both</option>
+</select><br>
+<p><strong>Enter year to compare popularity</strong></p><br>
+<select name="yr">
+<option value="1944">1944</option>
+<option value="1945">1945</option>
+<option value="1946">1946</option>
+<option value="1947">1947</option>
+<option value="1948">1948</option>
+<option value="1949">1949</option>
+<option value="1950">1950</option>
+<option value="1951">1951</option>
+<option value="1952">1952</option>
+<option value="1953">1953</option>
+<option value="1954">1954</option>
+<option value="1955">1955</option>
+<option value="1956">1956</option>
+<option value="1957">1957</option>
+<option value="1958">1958</option>
+<option value="1959">1959</option>
+<option value="1960">1960</option>
+<option value="1961">1961</option>
+<option value="1962">1962</option>
+<option value="1963">1963</option>
+<option value="1964">1964</option>
+<option value="1965">1965</option>
+<option value="1966">1966</option>
+<option value="1967">1967</option>
+<option value="1968">1968</option>
+<option value="1969">1969</option>
+<option value="1970">1970</option>
+<option value="1971">1971</option>
+<option value="1972">1972</option>
+<option value="1973">1973</option>
+<option value="1974">1974</option>
+<option value="1975">1975</option>
+<option value="1976">1976</option>
+<option value="1977">1977</option>
+<option value="1978">1978</option>
+<option value="1979">1979</option>
+<option value="1980">1980</option>
+<option value="1981">1981</option>
+<option value="1982">1982</option>
+<option value="1983">1983</option>
+<option value="1984">1984</option>
+<option value="1985">1985</option>
+<option value="1986">1986</option>
+<option value="1987">1987</option>
+<option value="1988">1988</option>
+<option value="1989">1989</option>
+<option value="1990">1990</option>
+<option value="1991">1991</option>
+<option value="1992">1992</option>
+<option value="1993">1993</option>
+<option value="1994">1994</option>
+<option value="1995">1995</option>
+<option value="1996">1996</option>
+<option value="1997">1997</option>
+<option value="1998">1998</option>
+<option value="1999">1999</option>
+<option value="2000">2000</option>
+<option value="2001">2001</option>
+<option value="2002">2002</option>
+<option value="2003">2003</option>
+<option value="2004">2004</option>
+<option value="2005">2005</option>
+<option value="2006">2006</option>
+<option value="2007">2007</option>
+<option value="2008">2008</option>
+<option value="2009">2009</option>
+<option value="2010">2010</option>
+<option value="2011">2011</option>
+<option value="2012">2012</option>
+<option value="2013">2013</option>
+</select><br><br>
+<input type="submit" value="PRESS!!">
+</form>
+
+
+
+<?php
+include("phpgraphlib.php");
+$graph=new PHPGraphLib(550,350); 
+$conn_err='Couldnt connect!!!!!';
+$mysql_host='localhost';
+$mysql_user='root';
+$mysql_pass='';
+$mysql_db='baby1944';
+
+if(!mysql_connect($mysql_host,$mysql_user,$mysql_pass)||!mysql_select_db($mysql_db))
+{
+	die($conn_err);
+	}	
+$num=$_GET["num"];
+$g=$_GET["g"];
+$num++;
+$gender=$_GET["gender"];
+if($gender=="Boy")
+{
+	$m='m'.$_GET['year'];
+	
+		$query="SELECT name,Amount FROM $m LIMIT $num";
+        if($query_run=mysql_query($query))
+		{
+			while($query_row=mysql_fetch_assoc($query_run))
+			{
+				$name=$query_row['name'];
+				$amount=$query_row['Amount'];
+				$position=$query_run['Position'];
+				echo $name.' '.$amount.''.$position.'</br>';
+				
+				
+				
+			}
+		}
+		else
+			echo mysql_error();
+		
+	
+
+}
+else if($gender=="Girl")
+{
+$n=$_GET['year'];
+		$query="SELECT name,Amount FROM $n LIMIT $num";
+        if($query_run=mysql_query($query))
+		{
+			while($query_row=mysql_fetch_assoc($query_run))
+			{
+				$name=$query_row['name'];
+				$amount=$query_row['Amount'];
+				$position=$query_run['Position'];
+				echo $name.' '.$amount.''.$position.'</br>';
+				
+				
+				
+			}
+		}
+		else
+			echo mysql_error();		
+}
+else 
+{            echo "Girl baby name are:-<br>";
+$n=$_GET['year'];
+
+		$query="SELECT name,Amount FROM $n LIMIT $num";
+        if($query_run=mysql_query($query))
+		{
+			while($query_row=mysql_fetch_assoc($query_run))
+			{
+				$name=$query_row['name'];
+				$amount=$query_row['Amount'];
+				$position=$query_run['Position'];
+				echo $name.' '.$amount.''.$position.'</br>';
+				
+				
+				
+			}
+		}
+		
+		echo "Boy baby name are:-<br>";
+		
+	$m='m'.$_GET['year'];
+		$query="SELECT name,Amount FROM $m LIMIT $num";
+		
+        if($query_run=mysql_query($query))
+		{
+
+			while($query_row=mysql_fetch_assoc($query_run))
+			{
+				$name=$query_row['name'];
+				$amount=$query_row['Amount'];
+				$position=$query_run['Position'];
+				echo $name.' '.$amount.''.$position.'</br>';
+				
+				
+				
+			}
+		}
+		else
+			echo mysql_error();
+		
+	 
+
+}
+
+$y=$_GET["yr"];
+$m=strtoupper($_GET["nm"]);
+$x=array();
+$c1 =$y;
+
+while($y<=2013)
+{if($g=='Girl')
+	$t="baby".$y;
+else
+	$t="mbaby".$y;
+	
+ $a="SELECT Amount FROM $t WHERE name='$m'"; 
+ if($c=mysql_query($a))
+ {
+
+ while($queryrow=mysql_fetch_assoc($c))
+			{
+				
+				$n=$queryrow['Amount'];
+				
+				
+				$x[]=$n;
+		
+			
+ }}
+  $y++;
+  
+
+}
+
+
+
+?>
+
+
+<?php //if (count($x) > 0):
+//print_r($x);
+?>
+<h3>The popularity comparison is as-:</h3>
+<table border="1" cellspacing="2" cellpadding="20">
+  <thead>
+    <tr>
+      <th>Year<?php
+	  //echo implode('</th><th>', array_keys(current($x))); ?></th>
+	  <th>Amount<?php
+	  //echo implode('</th><th>', array_keys(current($x))); ?></th>
+    </tr>
+  </thead>
+  <tbody>
+<?php $a = $c1-1; foreach ($x as $row): array_map('htmlentities', $row); ?>
+    <tr>
+      <td><?php  echo $a = $a+1 ?></td>
+	  <td><?php echo $row ?></td>
+    </tr>
+<?php endforeach; ?>
+  </tbody>
+</table>
+<?php //endif; ?>
+</body>
+</html>
